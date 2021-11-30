aded0d3b87201e63225455101ccc8f25818464d2
Christian G <chrisgobin94@gmail.com>
2021-11-30 12:49:53 -0500

    update

--- SolidityBasics/DataStorage.sol
+++ SolidityBasics/DataStorage.sol
@@ -33,4 +33,31 @@ contract DataStorage {
     function retrieveMyInt() public view returns(int) {
         return myInt;
     }
+    
+    //Create Struct and initialize
+    struct Person {
+        uint age;
+        string name;
+    }
+
+    Person public mike = Person({age: 32, name: "Mike"});
+    
+    // Dynamic Array called classroom consisting of Person structs
+    Person[] public classroom;
+    
+    // Attempt to populate the array.
+    function populateClassroom(uint256 _age, string memory _name) public {
+        uint256 count = 0;
+        while(count < 16){
+            classroom.push(Person({age: 23, name: "Kid"}));
+            count += 1;
+        }
+    }
+
+    // Attempt to show array values.
+    function showClassRoster() public returns(string){
+        for(uint x = 1; x <= 15; x++){
+            return(classroom.name[x]);
+        }
+    
 }
