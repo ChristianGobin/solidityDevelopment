c2a682d79d47056c6cf9f6c69999fa15be31fbf8
Christian G <chrisgobin94@gmail.com>
2021-12-15 13:23:22 -0500

    Update DataStorageFactory.sol

--- SolidityBasics/DataStorageFactory.sol
+++ SolidityBasics/DataStorageFactory.sol
@@ -1,7 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 import "./DataStorage.sol";
-
-contract DataStorageFactory{
     //Deploy Original Data Storage Contract through this Contract.
+    //Need to link original SimpleStorage.sol contract to this contract.
+    //Objective = deploy contract through this contract.
+    //Inheritance 
+    
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+import "./SimpleStorage.sol";
+
+contract CallExternal {
+    SimpleStorage public myStorage;
+    function returnSomeData() public view returns(uint256, string memory){
+        return(myStorage.getAuthorDetails());
+    }
 }
