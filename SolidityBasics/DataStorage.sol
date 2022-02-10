3c7ff66533266a64da2802c8609255a437e11ff9
Christian G <chrisgobin94@gmail.com>
2022-02-10 10:33:43 -0500

    Update DataStorage.sol

--- SolidityBasics/DataStorage.sol
+++ SolidityBasics/DataStorage.sol
@@ -1,48 +1,22 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+//SPDX-License-Identifier: MIT
+//Contract uses Structs, Arrays, Functions to populate an array of said Structs.
+pragma solidity >=0.8.0;
+contract DataStorage {
 
-contract SimpleStorage {
-    uint256 private tokenCount = 1_000_000;
-    string private author = "Christian";
-    
-    //Structs = custom data types with properties defined by user
-    struct Users {
-        string username;
-        uint256 age;
-        int debt;
-        uint currentHoldings;
-    }
-    
-    //Array of users
-    Users[] public myUsers;
-
-    //Populate Array
-    function populateArray(uint256 _age, string memory _name) public {
-        myUsers.push(Users(_name, _age));
+    struct Person {
+        string firstName;
+        string lastName;
+        uint8 age;
     }
 
+    Person[] private listOfPeople;
 
-    function showTokenCount() public view returns(uint256){
-        return tokenCount;
+    function addPersonToList(string memory _first, string memory _last, uint8 _age) public {
+        listOfPeople.push(Person(_first, _last, _age));
     }
 
-    function showAuthor() public view returns(string memory){
-        return author;
+    function showPersonByIndex(uint8 _index) public view returns(Person memory){
+        return listOfPeople[_index];
     }
-
-    // Mapping -> basically a dictionary
-    mapping (string => string) public actualDictionary;
-    
-    function addToActual(string memory word, string memory definition) public {
-        actualDictionary[word] = definition;
-    }
-
-    //Integer Array
-    uint256[] public integers;
-    
-    function addToIntegerArray(uint256 _int) public {
-        integers.push(_int);
-    }
-
 
 }
