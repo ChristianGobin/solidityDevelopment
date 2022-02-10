533de7739bfe0fe1d2942714ab09bd052190bb77
Christian G <chrisgobin94@gmail.com>
2022-02-10 12:36:51 -0500

    Added Mapping

--- SolidityBasics/DataStorage.sol
+++ SolidityBasics/DataStorage.sol
@@ -10,13 +10,19 @@ contract DataStorage {
     }
 
     Person[] private listOfPeople;
-
+    
+    // Map Person to their last name
+    mapping(string => string) public fullName;
+    
     function addPersonToList(string memory _first, string memory _last, uint8 _age) public {
         listOfPeople.push(Person(_first, _last, _age));
+        //When a person is added to list, map their first name to last name.
+        fullName[_first] = _last;
     }
 
     function showPersonByIndex(uint8 _index) public view returns(Person memory){
         return listOfPeople[_index];
+       
     }
 
 }
