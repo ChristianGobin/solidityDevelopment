b88ba45613cf4b69ebd7115a94277b30a41bbc5d
Christian G <chrisgobin94@gmail.com>
2022-02-11 10:13:27 -0500

    Update DataStorage.sol

--- SolidityBasics/DataStorage.sol
+++ SolidityBasics/DataStorage.sol
@@ -9,15 +9,18 @@ contract DataStorage {
         uint8 age;
     }
 
-    Person[] private listOfPeople;
+    //Array Version -> Person[] private listOfPeople;
+    //Use a mapping instead of array
+    uint countOfPeople = 0;
+    mapping(uint => Person) public listOfPeople;
     
     // Map Person to their last name
     mapping(string => string) public fullName;
     
     function addPersonToList(string memory _first, string memory _last, uint8 _age) public {
-        listOfPeople.push(Person(_first, _last, _age));
-        //When a person is added to list, map their first name to last name.
-        fullName[_first] = _last;
+        //When function fires, increment amount of people in mapping.
+        countOfPeople += 1;
+        listOfPeople[countOfPeople] = Person(_first, _last, _age));
     }
 
     function showPersonByIndex(uint8 _index) public view returns(Person memory){
