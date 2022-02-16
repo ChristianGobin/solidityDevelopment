7eeceb06952f220af0f09cf9bda7e3d2634d1a68
Christian G <chrisgobin94@gmail.com>
2022-02-15 20:58:46 -0500

    Syntax

--- SolidityBasics/DataStorage.sol
+++ SolidityBasics/DataStorage.sol
@@ -20,7 +20,7 @@ contract DataStorage {
     function addPersonToList(string memory _first, string memory _last, uint8 _age) public {
         //When function fires, increment amount of people in mapping.
         countOfPeople += 1;
-        listOfPeople[countOfPeople] = Person(_first, _last, _age));
+        listOfPeople[countOfPeople] = Person(_first, _last, _age);
     }
 
     function showPersonByIndex(uint8 _index) public view returns(Person memory){
