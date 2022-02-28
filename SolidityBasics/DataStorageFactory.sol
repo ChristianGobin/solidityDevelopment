6cc124e59c00ac8ed696b6a5988d79e60172e7de
Christian G <chrisgobin94@gmail.com>
2022-02-28 12:52:24 -0500

    Update DataStorageFactory.sol

--- SolidityBasics/DataStorageFactory.sol
+++ SolidityBasics/DataStorageFactory.sol
@@ -6,17 +6,7 @@ import './DataStorage.sol';
 //Need to link original SimpleStorage.sol contract to this contract.
 //Inheritance 
 
-contract StorageFactory{
-    string newPersonName;
-    string newPersonNameT;
-    uint8 anotherNum;
-
-    constructor(){
-        newPersonName = "Chris from StorageFactor";
-        newPersonNameT = "Chris again";
-        anotherNum = 54;
-    }
-    
+contract StorageFactory {
     //Array keeps track of Storage contracts.
     DataStorage[] public DataStorageArray;
     
