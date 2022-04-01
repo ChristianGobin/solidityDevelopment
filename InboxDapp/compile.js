bc5c9463c5e0099a4343f7b0585ca0a8848f359e
Christian G <chrisgobin94@gmail.com>
2022-04-01 12:51:34 -0400

    Update compile.js

--- InboxDapp/compile.js
+++ InboxDapp/compile.js
@@ -2,7 +2,11 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = //path.something(__dirname, InboxDapp, Inbox); //Link Path here.
-const source  = //fs.something(inboxPath, 'utf8'); //Convert file to utf encoded js
-console.log(solc(source, 1); compile 1 contract in the source code.
+//Path of Inbox.sol, contract.
+let inboxPath = path.resolve(__dirname, 'contracts', './Inbox.sol');
 
+//Source code of the contract
+let source = fs.readFileSync(inboxPath, 'utf8');
+
+let compiledCode = solc.compile(source, 1);
+console.log(compiledCode);
