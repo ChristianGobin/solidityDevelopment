a7e394f5b11598306b8c128bfd77674414ecb494
Christian G <chrisgobin94@gmail.com>
2022-04-01 02:17:31 -0400

    Boilerplate for local solidity development

---
+++ InboxDapp/compile.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = //path.something(__dirname, InboxDapp, Inbox); //Link Path here.
+const source  = //fs.something(inboxPath, 'utf8'); //Convert file to utf encoded js
+console.log(solc(source, 1); compile 1 contract in the source code.
+
