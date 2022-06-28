feb568f962c129f7451d6cc82c70283c3aa618b8
Christian G <chrisgobin94@gmail.com>
2022-06-28 07:49:47 -0400

    Created FundMe

---
+++ FundMe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FundMe {
+    address public owner;
+    mapping(address => uint256) public addressToBalance;
+
+    // Initialize the contract owner and cast it as a payable address
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    // Keep track of how much ether is sent to the address of this contract.
+    function fund() public payable {
+        addressToBalance[msg.sender] += msg.value;
+    }
+}
