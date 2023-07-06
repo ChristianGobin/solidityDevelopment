ec1c5d9bebd9a81912d2ee3086306d3959607eba
Christian G <chrisgobin94@gmail.com>
2023-07-06 10:52:27 -0400

    Create terminalGameMoveFunc.sol
    created solidity solution file for codewars exercise.

---
+++ terminalGameMoveFunc.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  function move(int p, int r) public pure returns (int) {
+    int solution = p + (r * 2);
+    return solution;
+  }
+}
