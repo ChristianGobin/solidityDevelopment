b107d23863ac2fe4d5bf9a5a7b283ef97d9a3ddb
Christian G <chrisgobin94@gmail.com>
2023-11-25 19:32:16 -0500

    Update StorageFactory.sol

--- SimpleStorageEth/StorageFactory.sol
+++ SimpleStorageEth/StorageFactory.sol
@@ -1,7 +1,12 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
+import './SimpleStorage.sol';
 
 pragma solidity ^0.8.0;
 
 contract StorageFactory {
-    
+    SimpleStorage[] public sfFactory;
+
+    function deployToSf() public {
+        sfFactory.push(SimpleStorage);
+    }
 }
