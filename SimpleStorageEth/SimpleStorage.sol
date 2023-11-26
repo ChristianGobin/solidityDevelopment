b4fbd45fd6e22a2173bb7d088a32b2fa4839474c
Christian G <chrisgobin94@gmail.com>
2023-11-25 19:31:55 -0500

    Update SimpleStorage.sol

--- SimpleStorageEth/SimpleStorage.sol
+++ SimpleStorageEth/SimpleStorage.sol
@@ -1,55 +1,15 @@
 // SPDX-License-Identifier: MIT
-
-// pragma statement sets compiler version
 pragma solidity ^0.8.0;
 
-// initial SimpleStorage contract creation
 contract SimpleStorage {
-    
-    // global state variable of index, and Study struct declared
-    uint index;
+    Person[] public personArray;
 
-    struct Study {
-        string programmingLanguage;
-        string concentration;
+    struct Person {
+        uint256 age;
+        string name;
     }
 
-    // set index on creation of contract
-    constructor(){
-        index = 1;
-    }
-
-    // create mapping data type to keep track of new Study's added to StudyMapping
-    // map according to index
-    mapping(uint => Study) StudyMapping;
-
-    // get Study data type by passing index int through to mapping
-    function getStudyByIndex(uint _index) public returns(Study memory){
-        
-        // check that _index is within range of global index
-        require(
-            _index >= index,
-            "Index is out of bounds!"
-            );
-
-        // set return value equal to Study at the given _index   
-        Study memory reVal = StudyMapping[index];
-
-        // iterate index variable after new Study is added to mapping
-        index++;
-
-        return(reVal);
-
-    }
-
-    // add new Study struct to map.
-    function addStudyToMap(string memory _progammingLanguage, string memory _concentration) public returns(uint){
-        StudyMapping[index] = Study(_progammingLanguage, _concentration);
-        return 0;
+    function addToPersonArray(uint256 _age, string memory _name) public {
+        personArray.push(Person(_age, _name));
     }
 }
-
-/*
-    Contract Summary: 
-    Creates a Custom Study Structure and adds Struct to Mapping using [Index => Struct] as the Value Pair.
-*/
