e6a73c0c0b92aaeed9260728c6822f6f3112da93
Christian G <chrisgobin94@gmail.com>
2024-03-13 21:52:11 -0400

    Create stringRepeat.sol

---
+++ stringRepeat.sol
@@ -0,0 +1,13 @@
+// problem = https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Repeater {
+  function multiply(uint8 repeat, string memory pattern) public pure returns (string memory) {
+    string[] answer;
+    whil
+      answer += pattern;
+    }
+    return answer;
+  }
+}
